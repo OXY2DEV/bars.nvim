b86e5574fc0ec89963e4264f5704045dc3736bd5
Shawon <mdmoinulhossainshawon@gmail.com>
2025-02-07 19:09:29 +0600

    refactor(core): Added ability to globally set bars & lines

--- plugin/bars.lua
+++ plugin/bars.lua
@@ -1,5 +1,10 @@
+--- Utility functions.
 local utils = require("bars.utils");
+
+--- Load all the global functions.
 require("bars.global");
+
+---|fS "Cache default values."
 
 vim.g.__statusline = utils.constant(vim.o.statusline);
 
@@ -9,6 +14,18 @@ vim.g.__statuscolumn = utils.constant(vim.o.statuscolumn);
 
 vim.g.__winbar = utils.constant(vim.o.winbar);
 vim.g.__tabline = utils.constant(vim.o.tabline);
+
+---|fE
+
+if require("bars").config.global == true then
+	require("bars.statuscolumn").global_attach();
+	require("bars.statusline").global_attach();
+	require("bars.winbar").global_attach();
+
+	require("bars.tabline").attach();
+else
+	require("bars.tabline").attach();
+end
 
 vim.api.nvim_create_autocmd({
 	"VimEnter",
@@ -22,11 +39,15 @@ vim.api.nvim_create_autocmd({
 		require("bars.winbar").clean();
 		require("bars.tabline").clean();
 
-		for _, win in ipairs(vim.api.nvim_list_wins()) do
-			require("bars.statusline").attach(win);
-			require("bars.statuscolumn").attach(win);
-			require("bars.winbar").attach(win);
-		end
+		vim.schedule(function ()
+			for _, win in ipairs(vim.api.nvim_list_wins()) do
+				require("bars.statusline").attach(win);
+				require("bars.statuscolumn").attach(win);
+				require("bars.winbar").attach(win);
+			end
+
+			require("bars.tabline").attach();
+		end)
 	end
 });
 
@@ -45,12 +66,14 @@ vim.api.nvim_create_autocmd({ "OptionSet" }, {
 		require("bars.winbar").clean();
 		require("bars.tabline").clean();
 
-		--- Attach new windows.
-		for _, win in ipairs(vim.api.nvim_list_wins()) do
-			require("bars.statusline").attach(win);
-			require("bars.statuscolumn").attach(win);
-			require("bars.winbar").attach(win);
+		vim.schedule(function ()
+			for _, win in ipairs(vim.api.nvim_list_wins()) do
+				require("bars.statusline").attach(win);
+				require("bars.statuscolumn").attach(win);
+				require("bars.winbar").attach(win);
+			end
+
 			require("bars.tabline").attach();
-		end
+		end)
 	end
 });
