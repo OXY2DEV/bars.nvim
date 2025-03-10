5d6a0ea8fe2341c8374f0f05190d5112d742032b
Shawon <mdmoinulhossainshawon@gmail.com>
2025-03-10 16:06:28 +0600

    doc(statusline): Added type definitions

--- lua/bars/statusline.lua
+++ lua/bars/statusline.lua
@@ -1,3 +1,4 @@
+--- Custom statusline module.
 local statusline = {};
 local components = require("bars.components.statusline");
 
@@ -5,7 +6,6 @@ local components = require("bars.components.statusline");
 ---@type string
 local STL = "%!v:lua.require('bars.statusline').render()";
 
---- Configuration table.
 ---@type statusline.config
 statusline.config = {
 	ignore_filetypes = {},
@@ -418,26 +418,6 @@ statusline.config = {
 
 		---|fE
 	}
-	-- ["lua"] = {
-	-- 	condition = function (buffer)
-	-- 		local ft = vim.bo[buffer].ft;
-	-- 		return ft == "lua";
-	-- 	end,
-	-- 	parts = {
-	-- 		{
-	-- 			kind = "mode",
-	--
-	-- 			default = {
-	-- 				padding_left = " ",
-	-- 				padding_right = " ",
-	-- 				icon = " ",
-	--
-	-- 				hl = "Special"
-	-- 			},
-	-- 			n = { text = "Normal" },
-	-- 		}
-	-- 	}
-	-- }
 };
 
 ---@type statusline.state
@@ -521,7 +501,12 @@ statusline.render = function ()
 	---|fE
 end
 
+--- Can we detach from {win}?
+---@param win integer
+---@return boolean
 statusline.can_detach = function (win)
+	---|fS
+
 	if vim.api.nvim_win_is_valid(win) == false then
 		return false;
 	end
@@ -547,6 +532,8 @@ statusline.can_detach = function (win)
 			return false;
 		end
 	end
+
+	---|fE
 end
 
 --- Detaches from {buffer}.
@@ -597,7 +584,13 @@ statusline.detach = function (window)
 	---|fE
 end
 
+--- Can we attach to {win}?
+---@param win integer
+---@param force boolean Forcefully attach window whose state is `false`.
+---@return boolean
 statusline.can_attach = function (win, force)
+	---|fS
+
 	if type(win) ~= "number" or vim.api.nvim_win_is_valid(win) == false then
 		return false;
 	elseif force ~= true and statusline.state.attached_windows[win] == false then
@@ -627,6 +620,8 @@ statusline.can_attach = function (win, force)
 			return true;
 		end
 	end
+
+	---|fE
 end
 
 --- Attaches the statusline module to the windows
@@ -658,6 +653,8 @@ end
 
 --- Attaches globally.
 statusline.global_attach = function ()
+	---|fS
+
 	if statusline.state.enable == false then
 		return;
 	end
@@ -668,6 +665,8 @@ statusline.global_attach = function ()
 
 	vim.g.__statusline = vim.o.statusline == STL and "" or vim.o.statusline;
 	vim.o.statusline = STL;
+
+	---|fE
 end
 
 --- Cleans up invalid buffers and recalculates
@@ -691,15 +690,21 @@ end
 --- Enables statusline for `window`.
 ---@param window integer
 statusline.enable = function (window)
+	---|fS
+
 	if type(window) ~= "number" or statusline.state.attached_windows[window] == nil then
 		return;
 	end
 
 	statusline.attach(window, true);
+
+	---|fE
 end
 
 --- Enables *all* attached windows.
 statusline.Enable = function ()
+	---|fS
+
 	statusline.state.enable = true;
 
 	for window, state in pairs(statusline.state.attached_windows) do
@@ -707,20 +712,28 @@ statusline.Enable = function ()
 			statusline.enable(window);
 		end
 	end
+
+	---|fE
 end
 
 --- Disables statusline for `window`.
 ---@param window integer
 statusline.disable = function (window)
+	---|fS
+
 	if type(window) ~= "number" or statusline.state.attached_windows[window] == nil then
 		return;
 	end
 
 	statusline.detach(window);
+
+	---|fE
 end
 
 --- Disables *all* attached windows.
 statusline.Disable = function ()
+	---|fS
+
 	for window, state in pairs(statusline.state.attached_windows) do
 		if state ~= false then
 			statusline.disable(window);
@@ -728,6 +741,8 @@ statusline.Disable = function ()
 	end
 
 	statusline.state.enable = false;
+
+	---|fE
 end
 
 ----------------------------------------------------------------------
@@ -735,6 +750,8 @@ end
 --- Toggles state of given window.
 ---@param window integer
 statusline.toggle = function (window)
+	---|fS
+
 	if type(window) ~= "number" or statusline.state.attached_windows[window] == nil then
 		return;
 	elseif statusline.state.attached_windows[window] == true then
@@ -742,15 +759,21 @@ statusline.toggle = function (window)
 	else
 		statusline.enable(window);
 	end
+
+	---|fE
 end
 
 --- Toggles statusline **globally**.
 statusline.Toggle = function ()
+	---|fS
+
 	if statusline.state.enable == true then
 		statusline.Disable();
 	else
 		statusline.Enable();
 	end
+
+	---|fE
 end
 
 ----------------------------------------------------------------------
@@ -758,6 +781,8 @@ end
 --- Sets up the statusline module.
 ---@param config statusline.config | boolean | nil
 statusline.setup = function (config)
+	---|fS
+
 	if type(config) == "table" then
 		statusline.config = vim.tbl_extend("force", statusline.config, config);
 	elseif type(config) == "boolean" then
@@ -767,6 +792,8 @@ statusline.setup = function (config)
 	for window, _ in pairs(statusline.state.attached_windows) do
 		statusline.update_id(window);
 	end
+
+	---|fE
 end
 
 return statusline;
