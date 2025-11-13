860232778dba9d46513cbad19d3a2b24586a3ffc
Shawon <mdmouinulhossainshawon@gmail.com>
2025-11-13 09:42:48 +0600

    doc: Updated type defs

--- lua/bars/types/statuscolumn.lua
+++ lua/bars/types/statuscolumn.lua
@@ -1,227 +1,121 @@
 ---@meta
 
---- Statuscolumn state variables.
----@class statuscolumn.state
----
---- Is the module enabled?
----@field enable boolean
----
---- State for attached windows,
----   true -> Enabled(should be rendered).
----   false -> Attached but disabled.
----
----@field attached_windows table<integer, boolean>
-
------------------------------------------------------------------------------
-
 --- Configuration table for the statuscolumn.
 ---@class statuscolumn.config
 ---
----@field force_attach? string[] List of `statuscolumns`s to ignore when attaching.
+---@field force_attach? string[] List of `statuscolumn` values to ignore when attaching.
 ---
---- Filetypes to ignore.
----@field ignore_filetypes string[]
---- Buftypes to ignore.
----@field ignore_buftypes string[]
+---@field ignore_filetypes string[] Filetypes to ignore when attaching.
+---@field ignore_buftypes string[] Buffer types to ignore when attaching.
 ---
---- Additional condition for attaching to
---- windows.
----@field condition? fun(buffer: integer, window: integer): boolean | nil
+---@field condition? fun(buffer: integer, window: integer): boolean Additional condition for attaching to windows.
 ---
---- Default style.
----@field default statuscolumn.style
----
---- Configuration style.
----@field [string] statuscolumn.style
+---@field default statuscolumn.style Default style.
+---@field [string] statuscolumn.style Named style.
 
 
---- A configuration style.
---- Must have a condition(unless `default`)
---- and a list of components.
+--[[ Style for the `statuscolumn`. ]]
 ---@class statuscolumn.style
 ---
---- Condition for this style.
----@field condition? fun(buffer: integer, window: integer): boolean
----
---- Components for this style.
----@field components statuscolumn_component[]
+---@field condition? fun(buffer: integer, window: integer): boolean Condition for this style.(unused when style is `default`)
+---@field components statuscolumn.component[] Components for this style.
 
 
----@alias statuscolumn_component
----| statuscolumn.components.lnum
----| statuscolumn.components.folds
----| statuscolumn.components.signs
----| statuscolumn.components.empty
----| statuscolumn.components.border
----| statuscolumn.components.custom
+---@alias statuscolumn.component
+---| statuscolumn.components.border Border.
+---| statuscolumn.components.custom Custom text.
+---| statuscolumn.components.empty Empty column.
+---| statuscolumn.components.folds Fold column.
+---| statuscolumn.components.lnum Line numbers.
+---| statuscolumn.components.signs Sign column.
 
 -----------------------------------------------------------------------------
 
---- Line number for statuscolumn.
+--[[ Line numbers. ]]
 ---@class statuscolumn.components.lnum
 ---
---- Condition for this component.
----@field condition? fun(buffer: integer, window: integer, statuscolumn: string): boolean
----
---- What kind of component is this?
 ---@field kind "lnum"
+---@field condition? fun(buffer: integer, window: integer, statuscolumn: string): boolean Condition for this component.
 ---
 ---@field click? boolean | fun(buffer: integer, window: integer, statuscolumn: string): boolean
 ---
 ---@field mode
----| 1 Line number.
+---| 1 Absolute line number.
 ---| 2 Relative line number.
 ---| 3 Hybrid line number.
 ---
---- Text used for the wrapped lines.
---- Can be a list to create a *fake* gradient effect.
----@field wrap_markers string | string[]
+---@field wrap_markers string | string[] Text used for the wrapped lines. Can be a list to change the character used for `n`th wrapped line.
+---@field virt_markers string | string[] Text used for the virtual lines. Can be a list to change the character used for `n`th virtual line.
 ---
---- Text used for the virtual lines.
---- Can be a list to create a *fake* gradient effect.
----@field virt_markers string | string[]
+---@field wrap_hl? string | string[] Highlight group for `wrap_markers`. Can be a list to create a *gradient effect*(relative to the main line).
+---@field virt_hl? string | string[] Highlight group for `virt_markers`. Can be a list to create a *gradient effect*(relative to the main line).
 ---
---- Highlight group for `wrap_markers`.
---- Can be a list to create a color gradient.
----@field wrap_hl? string | string[]
----
---- Highlight group for `virt_markers`.
---- Can be a list to create a color gradient.
----@field virt_hl? string | string[]
----
---- Highlight group for the line numbers.
---- Can be a list to create a color gradient.
----@field hl? string | string[] | fun(buffer: integer, window: integer): ( string | string[] )
+---@field hl? string | string[] | fun(buffer: integer, window: integer): ( string | string[] ) Highlight group for the line numbers. Can be a list to create a *gradient effect*(relative to current line).
 
 
---- Empty component.
+--[[ Empty column. ]]
 ---@class statuscolumn.components.empty
 ---
---- Condition for this component.
----@field condition? fun(buffer: integer, window: integer, statuscolumn: string): boolean
----
---- What kind of component is this?
 ---@field kind "empty"
+---@field condition? fun(buffer: integer, window: integer, statuscolumn: string): boolean Condition for this component.
 ---
---- How many columns should this span?
----@field width integer
----
---- Highlight group for this section.
----@field hl? string
+---@field width integer How many columns should this span?
+---@field hl? string Highlight group for this component.
 
 
---- Border for the statuscolumn.
+--[[ Border. ]]
 ---@class statuscolumn.components.border
 ---
---- Condition for this component.
----@field condition? fun(buffer: integer, window: integer, statuscolumn: string): boolean
----
---- What kind of component is this?
 ---@field kind "border"
+---@field condition? fun(buffer: integer, window: integer, statuscolumn: string): boolean Condition for this component.
 ---
---- Text to use for the border.
---- Can be a list to create a gradient.
----@field text string | string[]
----
---- Highlight group for the border.
---- Can be a list to create a gradient.
----@field hl? string | string[]
+---@field text string | string[] Text to use for the border. Can be a list to change the character used for `n`th line relative to the current line..
+---@field hl? string | string[] Highlight group for the border. Can be a list to change the highlight group used for `n`th line relative to the current line..
 
 
---- Fold column for the statuscolumn.
+--[[ Fold column. ]]
 ---@class statuscolumn.components.folds
 ---
---- Condition for this component.
----@field condition? fun(buffer: integer, window: integer, statuscolumn: string): boolean
----
---- What kind of component is this?
 ---@field kind "folds"
+---@field condition? fun(buffer: integer, window: integer, statuscolumn: string): boolean Condition for this component.
 ---
---- Text to show for closed fold.
---- Can be a list to use a different
---- text for different levels of folds.
----@field close_text string | string[]
+---@field close_text string | string[] Text used for indicating a closed fold. Can be a list to change the character used for the `n`th fold level.
+---@field close_hl? string | string[] Highlight group used for indicating a closed fold. Can be a list to change the Highlight group used for the `n`th fold level.
 ---
---- Highlight group for the `close_text`.
---- Can be a list to assign different
---- colors to different fold level.
----@field close_hl? string | string[]
+---@field open_text string | string[] Text used for indicating a opened fold. Can be a list to change the character used for the `n`th fold level.
+---@field open_hl? string | string[] Highlight group used for indicating a opened fold. Can be a list to change the Highlight group used for the `n`th fold level.
 ---
---- Text to show for opened fold.
---- Can be a list to use a different
---- text for different levels of folds.
----@field open_text string | string[]
+---@field scope_text string | string[] Text used for indicating the scope of a fold. Can be a list to change the character used for the `n`th fold level.
+---@field scope_hl? string | string[] Highlight group used for indicating the scope of a fold. Can be a list to change the Highlight group used for the `n`th fold level.
 ---
---- Highlight group for the `open_text`.
---- Can be a list to assign different
---- colors to different fold level.
----@field open_hl? string | string[]
+---@field scope_end_text string | string[] Text used for indicating the end of scope of a fold. Can be a list to change the character used for the `n`th fold level.
+---@field scope_end_hl? string | string[] Highlight group used for indicating the end of scope of a fold. Can be a list to change the Highlight group used for the `n`th fold level.
 ---
---- Text to show the scope of opened fold.
---- Can be a list to use a different
---- text for different levels of folds.
----@field scope_text string | string[]
+---@field scope_merge_text string | string[] Text used for indicating the change of fold level. Can be a list to change the character used for the `n`th fold level where the level is shifting from `n -> m`.
+---@field scope_merge_hl? string | string[] Highlight group used for indicating the change of fold level. Can be a list to change the highlight group used for the `n`th fold level where the level is shifting from `n -> m`.
 ---
---- Highlight group for the `scope_text`.
---- Can be a list to assign different
---- colors to different fold level.
----@field scope_hl? string | string[]
----
---- Text to show at the end of opened fold.
---- Can be a list to use a different
---- text for different levels of folds.
----@field scope_end_text string | string[]
----
---- Highlight group for the `scope_end_text`.
---- Can be a list to assign different
---- colors to different fold level.
----@field scope_end_hl? string | string[]
----
---- Text to show where fold level changes.
---- Can be a list to use a different
---- text for different levels of folds.
----@field scope_merge_text string | string[]
----
---- Highlight group for the `scope_merge_text`.
---- Can be a list to assign different
---- colors to different fold level.
----@field scope_merge_hl? string | string[]
----
---- Text for normal lines.
----@field fill_text string
----
---- Highlight group for `fill_text`.
----@field fill_hl? string
+---@field fill_text string Text for normal lines.
+---@field fill_hl? string Highlight group for `fill_text`.
 
 
---- Custom signcolumn.
+--[[ Sign column. ]]
 ---@class statuscolumn.components.signs
 ---
---- Condition for this component.
----@field condition? fun(buffer: integer, window: integer, statuscolumn: string): boolean
----
---- What kind of component is this?
 ---@field kind "signs"
+---@field condition? fun(buffer: integer, window: integer, statuscolumn: string): boolean Condition for this component.
 ---
---- Filter for signs.
----@field filter? fun(buffer: integer, ns_map: table<integer, string>, ns: integer, row: integer, col: integer, extmark: table): boolean
----
---- Highlight group for the signs.
---- NOTE, This will overwrite the
---- sign's original highlight group.
----@field hl? string
+---@field filter? fun(buffer: integer, ns_map: table<integer, string>, ns: integer, row: integer, col: integer, extmark: table): boolean Filter for signs.
+---@field hl? string Custom highlight group for the signs.
 
 
---- Custom component for the statuscolumn.
+--[[ Custom component. ]]
 ---@class statuscolumn.components.custom
 ---
---- Condition for this component.
----@field condition? fun(buffer: integer, window: integer, statuscolumn: string): boolean
----
---- What kind of component is this?
 ---@field kind "custom"
+---@field condition? fun(buffer: integer, window: integer, statuscolumn: string): boolean Condition for this component.
 ---
---- Text to render.
----@field value string | fun(buffer: integer, window: integer, statuscolumn: string): string
+---@field value
+---| string
+---| fun(buffer: integer, window: integer, statuscolumn: string): string
 
 
